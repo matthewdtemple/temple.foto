edd17806d0b4582f957f1ba32e99605c01096bb4
Matthew Temple <mattdtemple@gmail.com>
2023-05-13 22:04:27 -0500

    working with nav

---
+++ app/components/nav.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function Nav() {
+    return (
+        <div>
+            <a href="/about">About</a>
+            <a href="/images/landscape">Landscape</a>
+            <a href="/images/street">Street</a>
+        </div>
+    )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 // import './globals.css'
 import { Inter } from 'next/font/google'
+import Nav from './components/nav'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
-
+    <Nav></Nav>
     </html>
   )
 }
